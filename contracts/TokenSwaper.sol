e457e11314217dec71fdcebf0280d05f10f20985
TALOU Films <taloufilms@TALOUs-MBP.lan>
2023-08-23 02:15:39 -0400

    update token swapper basic template

--- contracts/TokenSwaper.sol
+++ contracts/TokenSwaper.sol
@@ -1,30 +1,72 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "./TokenSwapper.sol"; // Import the TokenSwapper contract
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
-contract Token is ERC20, Ownable, ERC20Permit {
-    uint256 public constant TOTAL_SUPPLY = 2171550050567400000000000000; // Total supply: 21,715,500,505.674
+contract TokenSwapper is Ownable {
+    address public marketingWallet = 0xYourMarketingWallet; // Placeholder: Replace with your marketing wallet address
+    IUniswapV2Router02 public uniswapRouter = IUniswapV2Router02(0xYourUniswapRouter); // Placeholder: Replace with Uniswap router address
+    address public targetTokenAddress = 0xYourTargetToken; // Placeholder: Replace with your target token address
 
-    address public tokenSwapperAddress = 0xYourTokenSwapperAddress; // Placeholder: Replace with your TokenSwapper contract address
+    event TokensSwapped(address indexed sender, uint256 amount);
+    event RewardsDistributed(address indexed marketingWallet, uint256 marketingAmount, uint256 remainingAmount);
 
-    constructor() ERC20("YourTokenName", "YourTokenSymbol") ERC20Permit("YourTokenName") {
-        transferOwnership(0xYourOwnerAddress); // Placeholder: Replace with the address you want to set as the owner
-        _mint(msg.sender, TOTAL_SUPPLY);
+    constructor() {}
+
+    function swapAndDistribute(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than 0");
+
+        // Swap tokens using Uniswap router
+        swapTokens(amount);
+
+        // Distribute rewards evenly to all token holders
+        distributeRewards();
+        
+        // Emit an event for the distribution
+        emit RewardsDistributed(marketingWallet, marketingAmount, remainingAmount);
     }
 
-    function _transfer(address sender, address recipient, uint256 amount) internal override {
-        uint256 taxAmount = (amount * 6) / 100; // 6% transfer tax
-        uint256 rewardsAmount = amount - taxAmount;
+    function swapTokens(uint256 amount) private {
+        IERC20 targetToken = IERC20(targetTokenAddress);
+        targetToken.transferFrom(msg.sender, address(this), amount);
 
-        // Transfer tax amount to the TokenSwapper contract
-        super._transfer(sender, tokenSwapperAddress, taxAmount);
+        address[] memory path = new address[](2);
+        path[0] = uniswapRouter.WETH();
+        path[1] = targetTokenAddress;
 
-        // Transfer rewards amount to the recipient
-        super._transfer(sender, recipient, rewardsAmount);
+        targetToken.approve(address(uniswapRouter), amount);
+
+        // Swap tokens on Uniswap
+        uniswapRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            amount,
+            0,
+            path,
+            address(this),
+            block.timestamp + 3600 // 1 hour deadline
+        );
+
+        emit TokensSwapped(msg.sender, amount);
+    }
+
+    function distributeRewards() private {
+        IERC20 targetToken = IERC20(targetTokenAddress);
+        uint256 totalSwapped = targetToken.balanceOf(address(this));
+        uint256 marketingAmount = (totalSwapped * 25) / 100;
+        uint256 remainingAmount = totalSwapped - marketingAmount;
+
+        uint256 totalHolders = targetToken.balanceOf(address(this)); // Placeholder: Replace with the actual count of token holders
+
+        // Distribute remaining balance evenly to all holders
+        for (uint256 i = 0; i < totalHolders; i++) {
+            address holder = targetToken.holderAtIndex(i); // Placeholder: Replace with the function to get token holder at index i
+            uint256 holderShare = (remainingAmount * targetToken.balanceOf(holder)) / totalHolders;
+            targetToken.transfer(holder, holderShare);
+        }
+
+        // Transfer 25% to marketing wallet
+        targetToken.transfer(marketingWallet, marketingAmount);
     }
 
     // Placeholder: Other contract functions and events
